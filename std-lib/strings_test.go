8b06ca10de4acb81c51477cc7e5584277dbf0209
Serj Parshin <serj.parshin@gmail.com>
2019-02-01 21:04:41 +0500

    add test search in empty str

--- std-lib/strings_test.go
+++ std-lib/strings_test.go
@@ -21,11 +21,24 @@ func TestContains(t *testing.T) {
 		assertCorrectTest(t, test, expected)
 	})
 
-	t.Run("expected false", func(t *testing.T) {
+	t.Run("space search", func(t *testing.T) {
 		test := strings.Contains("test", " ")
 		expected := false
 
 		assertCorrectTest(t, test, expected)
 	})
 
+	t.Run("search in empty string", func(t *testing.T) {
+		test := strings.Contains("", "es")
+		expected := false
+
+		assertCorrectTest(t, test, expected)
+	})
+
+}
+
+func BenchmarkContains(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.Contains("test", "es")
+	}
 }
