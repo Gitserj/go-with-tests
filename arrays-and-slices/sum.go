d520f56401e5385fd27c47de57c629f55c49d2ce
Serj Parshin <serj.parshin@gmail.com>
2019-02-03 22:49:07 +0500

    simple refactor

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,15 +1,9 @@
 package main
 
-import "fmt"
-
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
-}
-
-func main() {
-	fmt.Println("hola")
 }
