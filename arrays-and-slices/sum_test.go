a603cd326369c1de84df97a2d9ed0381b5e01664
Serj Parshin <serj.parshin@gmail.com>
2019-02-04 21:50:34 +0500

    simple refactor test

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -31,8 +31,8 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails([]int{1, 2}, []int{0, 9})
-	want := []int{2, 9}
+	got := SumAllTails([]int{1, 2, 4}, []int{0, 9, -1})
+	want := []int{6, 8}
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got '%v' want '%v'", got, want)
 	}
