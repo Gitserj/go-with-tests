8f411433bc6d8ddfdd6f5bf0219e6d7f2cbd00be
Serj Parshin <serj.parshin@gmail.com>
2019-02-04 14:21:16 +0500

    writen test for SumAll() func

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,7 @@
 package sum
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -17,15 +18,14 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	// t.Run("collection of any size", func(t *testing.T) {
-	// 	numbers := []int{1, 2, 3}
+}
 
-	// 	got := Sum(numbers)
-	// 	want := 6
-
-	// 	if got != want {
-	// 		t.Errorf("got '%d' want '%d', give '%v'", got, want, numbers)
-	// 	}
-	// })
-
+func TestSumAll(t *testing.T) {
+	t.Run("two slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9}, []int{1, 1})
+		want := []int{3, 9, 2}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%v' want '%v'", got, want)
+		}
+	})
 }
